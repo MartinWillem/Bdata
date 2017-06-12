769cba11d335100b6fd46ae495a496aaff17668f
vincent97 <vincent1997uithoorn@hotmail.com>
2017-06-12 11:12:10 +0200

    Update Atributen 8.0B.py

--- Atributen 8.0B.py
+++ Atributen 8.0B.py
@@ -1,35 +1,89 @@
+import os
 import re
 
+
+
+
+def super_lijst_maken():
+    file1 = open("Genes_relation.data.txt", "r")
+    alle_regels = file1.readlines()
+    file1.close()
+    PATTERN = re.compile(r'''((?:[^,"']|"[^"]*"|'[^']*')+)''')
+    super_lijst = [[], [], [], [], [], [], [], [], []]
+    for regel in alle_regels:
+        gesplitte_regel = PATTERN.split(regel.rstrip(".\r\n"))[1::2]
+        for i in range(0, 9):
+            if gesplitte_regel[i] != "?" and gesplitte_regel[i] != "Unknown":
+                super_lijst[i].append(gesplitte_regel[i])
+                super_lijst[i] = list(set(super_lijst[i]))
+    super_lijst[0] = ["label"]
+    return super_lijst
+
+def leuke_header_maken(super_lijst) :
+    string_header = ""
+    string_header2 = ""
+    for i in range(len(super_lijst)):
+        for x in super_lijst[i]:
+            string_header +=  x+","
+    for x in header2():
+        string_header2 += "Interactie met: " + x + "," + x + "correlatie coefficient" + ","
+    string_header = string_header + string_header2
+    string_header = string_header.rstrip(",")
+    string_header += "\n"
+    return string_header
+
+
+def header2():
+    with open('Genes_relation.data.txt', 'rw') as file:
+        genlijst = []
+        for line in file:
+            genlijst.append(line.split(",")[0])
+        genlijst = set(genlijst)
+        return genlijst
+def interacties():
+    dict = {}
+    with open('Interactions_relation.data.txt', 'rw') as file:
+        filelines = file.readlines()
+        tellijst = []
+        for i in filelines:
+            try:
+                dict[i.rstrip(".\r\n").split(",")[0]] += [i.rstrip(".\r\n").split(",")[1:4]]
+            except KeyError:
+                dict[i.rstrip(".\r\n").split(",")[0]] = [i.rstrip(".\r\n").split(",")[1:4]]
+        return dict
+
+
 def functie_1(super_lijst,header):
-    super_file = open("uber_file","w")
-    super_file.write(header)
     file1 = open("Genes_relation.data.txt","r")
     alle_regels = file1.readlines()
     file1.close()
     dict = interacties()
+    super_file = open("uber_file.csv","w")
+    super_file.write(header)
     eerste_gen = alle_regels[0]
     PATTERN = re.compile(r'''((?:[^,"']|"[^"]*"|'[^']*')+)''')
-    eerste_gen = PATTERN.split(eerste_gen)[1::2]
+    eerste_gen = PATTERN.split(eerste_gen.rstrip(".\r\n"))[1::2]
     lijst_van_al = [[],[],[],[],[],[],[],[],[]]
     for regel in alle_regels:
-        gesplitte_regel = PATTERN.split(regel)[1::2]
+        gesplitte_regel = PATTERN.split(regel.rstrip(".\r\n"))[1::2]
         if eerste_gen[0] != regel.split(",")[0]:
             toevoeg_string = eerste_gen[0]
             for lijst_nummer in range(1, 9):
                 for sl_woord in  super_lijst[lijst_nummer]:
                     for kaas in lijst_van_al[lijst_nummer]:
+                        ja = False
                         if sl_woord == kaas:
                             ja = True
+                            break
                         elif kaas == "?":
-                            ja = False
-                        else:
                             ja = "niet_chill"
+                            break
                     if ja == True:
                         toevoeg_string += ",True"
                     elif ja == False:
-                        toevoeg_string += ",?"
-                    else:
                         toevoeg_string += ",False"
+                    else:
+                        toevoeg_string += ",?"
             for genid in lijst_van_al[0]:
                 try:
                     print dict[genid]
@@ -50,75 +104,17 @@ def functie_1(super_lijst,header):
             eerste_gen = gesplitte_regel
             lijst_van_al = [[],[], [], [], [], [], [], [], []]
         for i in range(0, 9):
-            lijst_van_al[i].append(maakschoon(gesplitte_regel[i]))
+            lijst_van_al[i].append(gesplitte_regel[i])
             lijst_van_al[i] = list(set(lijst_van_al[i]))
     super_file.close()
 
 
-def header1():
-    with open('Genes_relation.names.txt','rw') as file:
-        lijst = []
-        super_lijst = []
-        lijst2 = []
-        for line in file:
-            if not line.isspace():
-                line = line.rstrip("\n ")
-                lijst.append(line.split("\r")[0])
-        for x in range(0, len(lijst)):
-            lijst[x] = re.sub(r'.*:', '', lijst[x])
-            lijst[x] = re.sub(r'\.', ',', lijst[x])
-            lijst[x] = lijst[x].rstrip(",")
-            lijst[x] = lijst[x].strip("\t")
-            lijst_klein = []
-            for ding in lijst[x].split(","):
-                lijst_klein.append(re.sub("\"", "", ding).strip(" "))
-            super_lijst.append(lijst_klein)
-    return(super_lijst)
-
-def header2():
-    with open('Genes_relation.data.txt', 'rw') as file:
-        genlijst = []
-        for line in file:
-            genlijst.append(line.split(",")[0])
-        genlijst = set(genlijst)
-        return genlijst
-
-def interacties():
-    dict = {}
-    with open('Interactions_relation.data.txt', 'rw') as file:
-        filelines = file.readlines()
-        tellijst = []
-        for i in filelines:
-            try:
-                dict[i.rstrip(".\r\n").split(",")[0]] += [i.rstrip(".\r\n").split(",")[1:4]]
-            except KeyError:
-                dict[i.rstrip(".\r\n").split(",")[0]] = [i.rstrip(".\r\n").split(",")[1:4]]
-        return dict
-
-def leuke_header_maken(super_lijst) :
-    string_header = ""
-    string_header2 = ""
-    for i in range(len(super_lijst)):
-        for x in super_lijst[i]:
-            string_header +=  x+","
-    for x in header2():
-        string_header2 += "Interactie met: " + x + "," + x + "correlatie coefficient" + ","
-    string_header = string_header + string_header2
-    string_header = string_header.rstrip(",")
-    string_header += "\n"
-    return string_header
-
-
-def maakschoon(niet_schoon):
-    niet_schoon = re.sub(",", "", niet_schoon)
-    niet_schoon = re.sub(r'(?:_[A-z])?\.([^.]*)$', "", niet_schoon)
-    schoon = re.sub('"', "", niet_schoon)
-    return schoon
 
 
 def main():
-    interacties()
-    super_lijst = header1()
-    header =leuke_header_maken(super_lijst)
+    super_lijst = super_lijst_maken()
+    header = leuke_header_maken(super_lijst)
     functie_1(super_lijst,header)
+
+
 main()
